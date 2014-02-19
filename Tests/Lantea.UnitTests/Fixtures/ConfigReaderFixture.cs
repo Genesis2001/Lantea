f9941e6430fa567a61e6036506516895ddd45578
Zack Loveless <bhmath@gmail.com>
2014-02-18 21:37:13 -0700

    Add test class/fixture for configreader

---
+++ Tests/Lantea.UnitTests/Fixtures/ConfigReaderFixture.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="ConfigReaderFixture.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Lantea.UnitTests.Fixtures
+{
+	using Core.IO;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class ConfigReaderFixture
+	{
+		private ConfigReader SUT;
+
+		[SetUp]
+		public void Setup()
+		{
+			SUT = new ConfigReader();
+		}
+
+
+	}
+}
