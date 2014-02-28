af0edd13bf0de0ad3a67aaaab3995b560a501c23
Zack Loveless <bhmath@gmail.com>
2014-02-27 17:48:34 -0700

    Add application boot loader.

--- Lantea/LanteaBot/Program.cs
+++ Lantea/LanteaBot/Program.cs
@@ -7,9 +7,14 @@
 namespace LanteaBot
 {
 	using System;
+	using System.ComponentModel.Composition;
+	using System.ComponentModel.Composition.Hosting;
+	using System.Reflection;
+	using System.Threading.Tasks;
 	using Lantea.Core.IO;
+	using NDesk.Options;
 
-	public class Program
+	public class Program : IDisposable
 	{
 		public static void Main(string[] args)
 		{
@@ -20,19 +25,72 @@ namespace LanteaBot
 
 			configuration.Load("example.conf.txt");
 
-			/*using (var bot = new Bot())
-			{
-				bot.LoadSettings("Settings.xml");
-				bot.Start();
 
-				Console.WriteLine("Press <ENTER> to terminate the bot.");
-				Console.ReadLine();
-			}*/
+			using (Program p = new Program())
+			{
+				p.Compose();
+				p.Run(args);
+			}
 
 #if DEBUG
 			Console.Write("Bot running in debug mode. Press <ENTER> to exit completely.");
 			Console.ReadLine();
 #endif
 		}
+
+		private readonly OptionSet parser = new OptionSet();
+		[Import] private IBotCore bot;
+
+		public void Compose()
+		{
+			var asm       = Assembly.GetAssembly(typeof(Program));
+			var catalog   = new AssemblyCatalog(asm);
+			var container = new CompositionContainer(catalog);
+
+			 bot = container.GetExportedValue<IBotCore>();
+		}
+
+		public async void Run(params string[] args)
+		{
+			string config = null;
+			parser.Add("config=", "", x => config = x);
+			parser.Parse(args);
+
+			if (String.IsNullOrEmpty(config))
+			{
+				Console.WriteLine("No configuration file discovered in command line. Terminating.");
+				Environment.Exit(2);
+			}
+
+			bot.Load(config);
+			bot.Initialize();
+
+			ConsoleKeyInfo key;
+			do
+			{
+				await Task.Delay(100);
+
+				key = Console.ReadKey(true);
+			} while (!(key.Modifiers == ConsoleModifiers.Control && key.Key == ConsoleKey.C));
+		}
+
+		#region Implementation of IDisposable
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing) return;
+
+			bot.Dispose();
+		}
+
+		#endregion
 	}
 }
