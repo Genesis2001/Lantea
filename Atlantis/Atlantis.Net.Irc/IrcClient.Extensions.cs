6e22e3720ef5087afc271bbc4386ffd9372ffc8a
Zack Loveless <bhmath@gmail.com>
2014-03-18 21:37:55 -0700

    Add extension method to compare prefixes.

---
+++ Atlantis/Atlantis.Net.Irc/IrcClient.Extensions.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="IrcClientExtensions.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Atlantis.Net.Irc
+{
+	using System;
+
+	public partial class IrcClient
+	{
+		public bool Compare(char prefixA, char prefixB)
+		{
+			return accessPrefixes.IndexOf(prefixA) > accessPrefixes.IndexOf(prefixB);
+		}
+	}
+}
