15a8ac4f9df02a0a31f5216ba3f3f9e9331c27fe
Zack Loveless <bhmath@gmail.com>
2014-04-02 15:04:16 -0700

    Prototype uptime module with a config.

---
+++ Extensions/Uptime/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTitle("Uptime")]
+[assembly: AssemblyDescription("A module to retrieve the uptime of a service or server.")]
+[assembly: AssemblyCompany("Zack Loveless")]
+[assembly: AssemblyProduct("Uptime Module")]
+[assembly: AssemblyCopyright("Copyright © Zack Loveless. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0")]
---
+++ Extensions/Uptime/Uptime.cs
@@ -0,0 +1,68 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="Uptime.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Uptime
+{
+	using System;
+	using System.ComponentModel.Composition;
+	using Lantea.Common.Extensibility;
+	using Lantea.Common.IO;
+
+	[Module(Author = "Zack Loveless", Name = ModuleName, Type = ModuleType.VENDOR, Version = ModuleVersion)]
+	public class Uptime : ModuleCore
+	{
+		private const String ModuleName = "Service Uptime Module";
+		private const String ModuleVersion = "1.0";
+
+		[ImportingConstructor]
+		public Uptime([Import] IBotCore bot, [Import] Configuration config) : base(bot, config)
+		{
+		}
+
+		#region Overrides of ModuleCore
+
+		public override string Author
+		{
+			get { return "Zack Loveless"; }
+		}
+
+		public override string Name
+		{
+			get { return ModuleName; }
+		}
+
+		public override string Version
+		{
+			get { return ModuleVersion; }
+		}
+
+		public override ModuleType Type
+		{
+			get { return ModuleType.VENDOR; }
+		}
+
+		#endregion
+
+		#region Overrides of ModuleCore
+
+		public override void Load()
+		{
+			Block uptimeBlock = Config.GetBlock("uptime");
+
+			if (uptimeBlock != null)
+			{
+				for (Int32 i = 0; i < uptimeBlock.CountBlock("server"); ++i)
+				{
+					// todo.
+				}
+			}
+
+			base.Load();
+		}
+
+		#endregion
+	}
+}
