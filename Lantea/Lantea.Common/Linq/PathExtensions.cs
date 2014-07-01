937c29ed9bba27546dc4ef49d4b8fb76297998c2
Zack Loveless <bhmath@gmail.com>
2014-06-30 18:36:12 -0700

    Misc file from last commit that didn't make it into the commit.

---
+++ Lantea/Lantea.Common/Linq/PathExtensions.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="PathExtensions.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Lantea.Common.Linq
+{
+    using System;
+    using System.IO;
+
+    public static class PathExtensions
+    {
+        /// <summary>
+        /// Gets an absolute path of a given path. Returns null if it's not a valid Unc URI.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException" />
+        public static string GetAbsolutePath(this string source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            Uri path;
+
+            if (!Uri.TryCreate(source, UriKind.RelativeOrAbsolute, out path))
+            {
+                return null;
+            }
+
+            return !path.IsAbsoluteUri ? Path.GetFullPath(source) : path.AbsolutePath;
+        }
+    }
+}
