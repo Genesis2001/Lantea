7f3a8d3e77b2f6bcbbdf25d08586d6224d4c748d
Zack Loveless <bhmath@gmail.com>
2014-08-04 13:19:06 -0700

    Add nupkg of the libraries used. Also update gitignore for committing the build directory but not the debug or release binaries.

---
+++ Tests/LocalizedTests/Program.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="Program.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace LocalizedTests
+{
+    using System;
+    using Atlantis.Net.Query;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TS3Client client = null;
+            try
+            {
+                client = new TS3Client
+                                   {
+                                       Host       = "ts.cncfps.com",
+                                       QueryPort  = 26425,
+                                       ServerPort = 9987,
+                                       Login      = "Genesis",
+                                       Password   = "Ij7IEvRN",
+                                   };
+
+                client.Connect();
+
+
+
+                Console.Write("Press <ENTER> to continue...");
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Disconnect();
+                }
+            }
+        }
+    }
+}
