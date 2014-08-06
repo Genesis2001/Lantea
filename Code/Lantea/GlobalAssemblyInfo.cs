de677562ab38011c8bd212601c934813afa44f81
Zack Loveless <bhmath@gmail.com>
2014-08-06 14:00:13 -0700

    Update Lantea's core library for plugins

--- Code/Lantea/GlobalAssemblyInfo.cs
+++ Code/Lantea/GlobalAssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyVersion("1.0.1")]
